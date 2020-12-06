91c36e1189c1630e8334919d8d9a6a5ccb5bd300
Tyson Hester <ironman9967@gmail.com>
2020-12-05 21:36:59 -0500

    .

--- src/index.js
+++ src/index.js
@@ -15,8 +15,20 @@ createModuleLoader({
         createSubject
     }),
     cpSpawn,
-    moduleName: process.env.NPX_APP_MODULE_NAME || 'ledsrv-test-docker-app',
-    delay: process.env.NPX_APP_DELAY || 15 * 60 * 1000
+    moduleName: process.env.AUTO_NPX_CONTAINER_MODULE_NAME || 'ledsrv-test-docker-app',
+    delay: process.env.AUTO_NPX_CONTAINER_DELAY || 15 * 60 * 1000,
+    debug: process.env.AUTO_NPX_CONTAINER_DEBUG || 0
 })
-.then(({ dispose }) => 
-    process.once('SIGINT', () => dispose().then(() => process.exit(0))))
+.then(({ debug, onCheck, dispose }) => {
+    let unsubOnCheck = () => {}
+    if (debug) {
+        const { unsubscribe } = onCheck(moduleMeta => 
+            console.log('version checked', { moduleMeta }))
+        unsubOnCheck = unsubscribe
+    }
+    process.once('SIGINT', () => {
+        unsubOnCheck()
+        dispose().then(() => process.exit(0))
+    })
+})
+    
--- src/module-loader/index.js
+++ src/module-loader/index.js
@@ -5,34 +5,51 @@ export const create = ({
     onNewVersion,
     onCheck,
     moduleName, 
-    delay = 15 * 60 * 1000
+    delay = 15 * 60 * 1000,
+    debug = 0
 }) => {
     let proc
+    const log = (...args) => debug
+        ? console.log.apply(null, args)
+        : void 0
+    log('version check interval', delay)
     const stop = () => {
         const prom = new Promise(r => proc.once('close', () => {
+            log('module closed')
             proc = void 0
             r()
         }))
+        log('stopping - sending SIGINT to module')
         proc.kill('SIGINT')
         return prom
     }
     const { unsubscribe: dispose } = onNewVersion(({ 
         moduleMeta: { moduleName, latest }
     }) => {
-        const start = () => proc = cpSpawn('npx', [ `${moduleName}@${latest}` ], {
-            stdio: [ 'ignore', 'inherit', 'inherit' ]
-        })
-        if (!proc) {
-            start()
+        log('new version', { moduleName, latest })
+        const start = () => {
+            log('spawning module')
+            proc = cpSpawn('npx', [ `${moduleName}@${latest}` ], {
+                stdio: [ 'ignore', 'inherit', 'inherit' ]
+            })
             return Promise.resolve()
+        }
+        if (!proc) {
+            log('first run')
+            return start()
         }
         return stop().then(start)
     })
     return monitor(moduleName, delay).then(interval => ({
+        debug: debug == 1,
         onCheck,
-        dispose: () => stop().then(() => {
-            dispose()
-            clearInterval(interval)
-        })
+        dispose: () => {
+            log('disposing...')
+            return stop().then(() => {
+                log('disposed')
+                dispose()
+                clearInterval(interval)
+            })
+        }
     }))
 }
