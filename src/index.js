82b3010a0f12983f5e77f322f15d751323812b72
Tyson Hester <ironman9967@gmail.com>
2020-12-05 22:57:18 -0500

    .

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ createModuleLoader({
     }),
     cpSpawn,
     moduleName: process.env.AUTO_NPX_CONTAINER_MODULE_NAME || 'ledsrv-test-docker-app',
-    delay: 15000 || process.env.AUTO_NPX_CONTAINER_DELAY || 15 * 60 * 1000,
-    killSignal: 'SIGTERM' || process.env.AUTO_NPX_CONTAINER_KILL_SIGNAL || 'SIGINT',
-    debug: 1 || process.env.AUTO_NPX_CONTAINER_DEBUG || 0
+    delay: process.env.AUTO_NPX_CONTAINER_DELAY || 15 * 60 * 1000,
+    killSignal: process.env.AUTO_NPX_CONTAINER_KILL_SIGNAL || 'SIGINT',
+    debug: process.env.AUTO_NPX_CONTAINER_DEBUG || 0
 })
 .then(({ debug, killSignal, onCheck, dispose }) => {
     let unsubOnCheck = () => {}
